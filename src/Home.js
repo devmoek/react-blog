5de31fe8e779a57485602d4df101f535bfa41d7e
Michael Myers <moek@devmoek.com>
2022-06-07 19:19:45 +0600

    episode 7

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,17 @@
 const Home = () => {
 
-    const handleClick = () => {
-        console.log("Hello!");
+    const handleClick = (e) => {
+        console.log("Hello!", e);
+    }
+
+    const handleClickAgain = (name) => {
+        console.log("Hello " + name);
     }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
             <button onClick={ handleClick }>Click me</button>
-            <button onClick={() => {
-                console.log('hello')
-            }}>Click me again</button>
+            <button onClick={() => handleClickAgain('MARIO')}>Click me again</button>
         </div>
      );
 }
